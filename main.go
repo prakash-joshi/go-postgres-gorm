c8faba1e41d59c89a9e7631bd1b87ee43c12f389
Prakash Joshi <prakash.joshi167@hotmail.com>
2024-06-01 20:37:20 +0530

    update main.go file

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/prakash-joshi/go-bookstore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoute(r)
+	http.Handle("/", r)
+	fmt.Println("Starting server on Port 8080....")
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+}
